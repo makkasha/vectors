294e77c4df5a42d02200f276df115121d78e8fcf
Mohamed Akkash <akkash-a@hm.edu>
2020-04-14 14:26:31 +0200

    add read.me file

---
+++ cmake-build-debug-mingw/CMakeFiles/vectors.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectors.dir/main.c.obj"
+  "CMakeFiles/vectors.dir/vectors.c.obj"
+  "libvectors.dll.a"
+  "vectors.exe"
+  "vectors.exe.manifest"
+  "vectors.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vectors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
