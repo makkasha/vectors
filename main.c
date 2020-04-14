a6cdca218ac43968cff22dcb9121ee070361331a
Mohamed Akkash <akkash-a@hm.edu>
2020-04-14 18:05:18 +0200

    added code for user input

--- main.c
+++ main.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "vectors.h"
 
 int main() {
+    int length_a, length_b = 0;
+    printf("Enter the length of vector a:\n");
+    if(!scanf("%d", &length_a)){
+        printf("Length of vector a can only be an integer! Try again!\n");
+        scanf("%d", &length_a);
+    }
+    double* a = (double*)calloc(length_a, sizeof(double));
+    for(int i = 0; i < length_a; i++){
+        printf("Enter a[%d]:\n", i);
+        scanf("%lf", (a + i));
+    }
 
-    double a[] = {1.1, 2.2, 3.3};
-    double b[] = {4.4, 5.5, 6.6};
+    printf("Enter the length of vector b:\n");
+    scanf("%d", &length_b);
+    double* b = (double*)calloc(length_b, sizeof(double));
+    for(int i = 0; i < length_b; i++){
+        printf("Enter b[%d]:\n", i);
+        scanf("%lf", (b + i));
+    }
 
     //Calculate dot product
-    double dot_prod = dot(a, b, sizeof(a)/sizeof(a[0]), sizeof(b)/sizeof(b[0]));
+    double dot_prod = dot(a, b, length_a, length_b);
     if(dot_prod){
         printf("Dot product of a and b: %f\n", dot_prod);
     }
 
     //Calculate the length of the vectors
-    printf("Length of a: %f\n", norm(a,sizeof(a)/sizeof(a[0])));
-    printf("Length of b: %f\n", norm(b,sizeof(b)/sizeof(b[0])));
+    printf("Length of a: %.10f\n", norm(a, length_a));
+    printf("Length of b: %.10f\n", norm(b, length_b));
 
     //Calculate the cross product of the vectors
     printf("Cross product of a and b:\n");
-    for(int i = 0; i < sizeof(a)/sizeof(a[0]); i++){
-        if(!cross(a, b, sizeof(a)/sizeof(a[0]), sizeof(b)/sizeof(b[0]))){
+    for(int i = 0; i < length_a; i++){
+        if(!cross(a, b, length_a, length_b)){
             break;
         };
-        double c = *(cross(a, b, sizeof(a)/sizeof(a[0]), sizeof(b)/sizeof(b[0]))+ i);
-        printf("c[%d]: %f\n", i, c);
+        double c = *(cross(a, b, length_a, length_a)+ i);
+        printf("c[%d]: %.10f\n", i, c);
     }
-
     return 0;
 }
